809a2d5543b1a6884e0be9c7ffe6f186fbea3e12
Oniichan3k <nguyenduchip1@gmail.com>
2024-10-29 21:41:01 +0700

    upd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path="/" element={<Navigate to="/signup" />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
-  </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
